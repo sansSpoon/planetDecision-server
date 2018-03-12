062fa8107d81ceed40e93231575622ec741fce8e
sansSpoon <sans.spoon@gmail.com>
2018-03-12 12:14:55 +1100

    update controller to use mongoose Model

--- server/api/users/controller.js
+++ server/api/users/controller.js
@@ -4,27 +4,57 @@
 ** TODO: Add try catch to everything?
 ** TODO: Need to determine how to seperate
 **   controller specific, or model specific functions
+** TODO: Update one record @ server - needs to have better promise handling
 **
 */
+
+
+// libraries
+const pluralize = require('pluralize');
+
+// loaders
+const Model = require('./model.js');
+
+// utilities
+const routePath = pluralize.singular(/[^/]*$/.exec(__dirname)[0]);
+
+
+// ! Get requested ID as parameter and attach to Koa context
+/*
+** @param id
+** @param ctx
+** @return {next}
+*/
+//
+exports.params = async function getParams(id, ctx, next) {
+
+	ctx[routePath] = await Model.findById(id);
+
+	if (!ctx.routePath) {
+
+		return (ctx.status = 404);
+	}
+	return next();
+};
 
 
 // ! Create new record
 /*
-** @param name
+** @param ctx
 ** @return {Promise}
 **
-** @example curl -i -XPOST '<host>:<port>/users' -d '{"firstName":"Jacob", "lastName":"Jones"}' -H 'Content-Type: application/json'
+** @example curl -i -XPOST '<host>:<port>/users' -d '{"firstName":"John", "lastName":"Doe"}' -H 'Content-Type: application/json'
 */
 //
 exports.createOne = async function createOne(ctx, next) {
 
-	ctx.body = await addToDB(ctx.params.id, ctx.request.body);
+	ctx.body = await Model.create(ctx.request.body).then((result) => result);
 	ctx.status = 201;
 	await next();
 };
 
 
-// ! Read one record from ID
+// ! Read one record
 /*
 ** @param id
 ** @return {Promise}
@@ -34,12 +64,12 @@ exports.createOne = async function createOne(ctx, next) {
 //
 exports.readOne = async function readOne(ctx, next) {
 
-	ctx.body = await getIdFromDb(ctx.params.id);
+	ctx.body = ctx.routePath;
 	await next();
 };
 
 
-// ! Read All records
+// ! Read all records
 /*
 ** @return {Promise}
 **
@@ -48,29 +78,58 @@ exports.readOne = async function readOne(ctx, next) {
 //
 exports.readAll = async function readAll(ctx, next) {
 
-	ctx.body = await getAllFromDb();
+	ctx.body = await Model.find();
 	await next();
 };
 
 
-// ! Update one records
+// ! Update one record @ client
 /*
-** @param id
-** @param firstName
-** @param lastName
+** @param ctx.body
 ** @return {Promise}
 **
-** @example curl -i -XPUT "<host>:<port>/users/<id>" -d '{"firstName":"Zoe", "lastName":"Zulu"}' -H 'Content-Type: application/json'
+** @example curl -i -XPUT "<host>:<port>/users/1" -d '{"firstName":"Zoe", "lastName":"Zulu"}' -H 'Content-Type: application/json'
 */
 //
 exports.updateOne = async function updateOne(ctx, next) {
 
-	ctx.body = await updateIdToDb(ctx.params.id, ctx.request.body);
+	Object.assign(ctx.routePath, ctx.request.body);
+
+	ctx.routePath.save((err, result) => {
+
+		if (err) {
+			next(err);
+		} else {
+			ctx.body = result;
+		}
+	});
+
 	await next();
 };
 
 
-// ! Delete one records
+// ! Update one record @ server (not attached to router, example only)
+/*
+** @param ctx.body
+** @return {Promise}
+**
+** @example curl -i -XPUT "<host>:<port>/users/1" -d '{"firstName":"Zoe", "lastName":"Zulu"}' -H 'Content-Type: application/json'
+*/
+//
+exports.updateOnServer = async function updateOnServer(ctx, next) {
+
+	Model.updateOne({
+		_id: ctx.params.id,
+	}, {
+		field1: ctx.request.body.field1,
+		$push: { field2: ctx.request.body.field2 },
+	}).exec();
+
+	await next();
+};
+
+
+// ! Delete one record
 /*
 ** @param id
 ** @return {Promise}
@@ -80,7 +139,8 @@ exports.updateOne = async function updateOne(ctx, next) {
 //
 exports.deleteOne = async function deleteOne(ctx, next) {
 
-	await removeIdInDb(ctx.params.id);
+	await Model.deleteOne(ctx.params.id);
 	ctx.status = 204;
+
 	await next();
 };
