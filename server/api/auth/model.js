354c524ffa30ed42a6c71cb5d8b356d59f8d2e5b
sansSpoon <sans.spoon@gmail.com>
2018-03-21 10:31:51 +1100

    Adding auth route from template

---
+++ server/api/auth/model.js
@@ -0,0 +1,9 @@
+'use strict';
+
+// ! Auth Model
+/*
+** Since auth route uses the Users route, there is no need
+** for a seperate auth model. However, the api-loader
+** requires each API route to have a model.js file.
+*/
+//
