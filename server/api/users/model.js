6987903df89460d135f7895a8db3787893d7738f
sansSpoon <sans.spoon@gmail.com>
2018-03-21 09:57:26 +1100

    update user schema, add validation

--- server/api/users/model.js
+++ server/api/users/model.js
@@ -10,14 +10,23 @@ const routePath = pluralize.singular(/[^/]*$/.exec(__dirname)[0]);
 // schema
 const Schema = mongoose.Schema; // eslint-disable-line
 const routeSchema = new Schema({
-	firstName: {
+	email: {
 		type: String,
 		required: true,
 	},
-	lastName: {
+	password: {
 		type: String,
 		required: true,
 	},
 }, { strict: 'throw' });
+
+// validation
+routeSchema.path('email').validate((email) => {
+	return /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/.test(email);
+}, 'A valid email address is required.');
+
+routeSchema.path('password').validate((password) => {
+	return /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*\W)(?!.* ).{8,}$/.test(password);
+}, 'Password must be greater than 7 characters, upper and lower case with digits and special characters.');
 
 module.exports = mongoose.model(routePath, routeSchema);
